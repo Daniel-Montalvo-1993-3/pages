9a0daa1186ec0fd3ce256f21df65c61e64d3ea0a
danielmmk <daniel.montalvo@mmkgroup.com.mx>
2026-05-27 19:52:15 -0600

    ajuste de eslint en variable

--- src/containers/HomeContainer/HomeContainer.tsx
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -153,7 +153,7 @@ export const HomeContainer: React.FC = () => {
               {dataLayerSnapshot.map((entry, index) => {
                 const eventName = typeof entry.event === 'string' ? entry.event : '';
                 const colorClass = EVENT_COLORS[eventName] ?? 'text-gray-300';
-                const { event: _event, ...rest } = entry;
+                const rest = Object.fromEntries(Object.entries(entry).filter(([k]) => k !== 'event'));
                 return (
                   <li
                     key={index}
